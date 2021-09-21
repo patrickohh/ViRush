5350a5583e04bc5c924cefa3e71437f8c2624d32
patrickohh <50275188+patrickohh@users.noreply.github.com>
2021-09-20 16:54:22 -0800

    Create Menu.hpp
    menu header file

---
+++ header/Menu.hpp
@@ -0,0 +1,18 @@
+/************************************************************************
+** Program name:  Menu Functions Declaration
+** Author:        Patrick Oh
+** Date:          06/11/19
+** Description:   This file contains function declarations for the
+**                functions that print out prompts and menus for the user
+**                throughout the game.
+************************************************************************/
+
+#ifndef FINAL_MENU_HPP
+#define FINAL_MENU_HPP
+
+void baseMenu(int&);
+void initialEnter(int);
+void repeatEnter(int);
+void playerMenu();
+
+#endif //FINAL_MENU_H
