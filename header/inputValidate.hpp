0692ebf861b847bf593577fd6abf2e9bd380f9a5
patrickohh <50275188+patrickohh@users.noreply.github.com>
2021-09-20 16:52:18 -0800

    Create inputValidate.hpp
    input validation header file

---
+++ header/inputValidate.hpp
@@ -0,0 +1,15 @@
+/************************************************************************
+** Program name:  Input Validation Functions Declarations
+** Author:        Patrick Oh
+** Date:          06/11/19
+** Description:   This file contains the input validation functions
+**                declarations.
+************************************************************************/
+#ifndef FINAL_INPUTVALIDATE_HPP
+#define FINAL_INPUTVALIDATE_HPP
+
+int choiceValidate();
+int choiceFourValidate();
+int choiceThreeValidate();
+
+#endif //FINAL_INPUTVALIDATE_H
